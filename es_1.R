9899e0e3158cdf51d0699378d4f5ab9842d9d86c
qwtehvn <deanst28@gmail.com>
2017-03-31 20:38:17 +0800

    Add files via upload

--- es_1.R
+++ es_1.R
@@ -1,32 +1,40 @@
 
 ############################################################
+############################################################
 
 library(foreign)
 es1 <- read.dta("C:/Users/CJY/Desktop/es/ES-Indicators-Database-.dta")
 #tfp <- read.dta("C:/Users/CJY/Desktop/es/Firm Level TFP Estimates and Factor Ratios_February_21_2017.dta")
 #attach(es1)
+qog1 <- read.dta("C:/Users/CJY/Desktop/es/qog_bas_ts_jan17.dta")
 
 ############################################################
 
+qog1$cy <- qog1$ccode*qog1$year
+es1$cy <- es1$ccode*es1$year
+
+mr1 <- merge(x = es1, y = qog1, by = "cy")
+
 ############################################################
+
 
 # http://www.statmethods.net/management/variables.html
 # http://www.cookbook-r.com/Manipulating_data/Recoding_data/
 
-es1$soe <- NULL
-es1$soe[es1$car4 >= 1] <- 0
-es1$soe[es1$car4 < 1] <- 2
-table(es1$soe)
+mr1$soe <- NULL
+mr$soe[mr1$car4 >= 1] <- 0
+mr1$soe[mr1$car4 < 1] <- 2
+table(mr1$soe)
 
 # http://www.statmethods.net/management/subset.html
 
-es2 <- es1[ which(es1$soe==0),]
-table(es2$soe)
-table(es2$size)
+es2 <- mr1[ which(mr1$car4 >= 1), ]
+
+es3 <- mr1[ which(mr1$region == "EAP"), ]
 
 wirte.csv(es2, file = "C:/Users/CJY/Desktop/es/es2.csv")
 write.dta(es2, file = "C:/Users/CJY/Desktop/es/es2.dta")
-
+write.dta(mr1, file = "C:/Users/CJY/Desktop/es/mr1.dta")
 
 table(es2$size)
 es2$size1 <- NA
@@ -39,9 +47,11 @@ table(es2$size1)
 
 table(es2$idstd)# firm id
 table(es2$year)# year
+table(es2$country2)#country
 
 table(es2$size)# company size
 table(es2$exporter)# exporter or now
+table(es2$ownership)# domestic or foreign
 
 table(es2$secfpct1)# ?
 
@@ -70,8 +80,8 @@ table(es2$obst13)# the biggest obstacle, tax administration
 table(es2$obst14)# the biggest obstacle, tax rates
 table(es2$obst15)# the biggest obstacle, transport
 
-
-
+table(es2$fin2)
+table(es2$stra_sector)
 
 ############################################################
 
@@ -79,19 +89,60 @@ library(plyr)
 
 ############################################################
 
+table1 <- table(es2$stra_sector, es2$obst8)
+table1
+
+table2 <- table
+
+############################################################
+
+
+############################################################
+
+es1 <- na.omit(es)
+table(es$soe)
+is.numeric(es$soe)
+
+class(es2$stra_sector)
+class(es2$exporter)
+
+############################################################
+
+library(lattice)
 library(ggplot2)
+
 comapnyage <- ggplot(es2, aes(es2$car1))+
-  geom_bar()+
+  geom_bar(aes(color = es2$car1))+
   coord_flip()+
+  scale_fill_manual(values = c("es2$car1"))+
   labs(x="company age")
 comapnyage
-ggsave("company age.png")
+ggsave("C:/Users/CJY/Desktop/es/company age.png")
+
+expper <- qplot(es2$perf1, es2$car6, data = es2, colour = factor(es2$exporter)) + 
+  scale_colour_manual(values = c("red","green"))
+expper
+
+boxplot(es2$perf1 ~ es2$country2)
+boxplot(es3$perf1 ~ es3$country2)
+barchart(es3$country2 ~ es3$perf1, data = es3)
+plot(es3$perf1, es3$car6)
+xyplot(es3$perf1 ~ es3$car6 | es3$country2, data = es3)
+
+perff1 <- ggplot(es3, aes(x = car6, y = perf1)) +
+  geom_point(aes(color = country2))
+perff1
+
+perff2 <- ggplot(es3, aes(x = country2, y = perf1)) +
+  geom_boxplot()
+perff2
+
+
+
+############################################################
 
 ggplot(es2, aes(x=es2$obst1)) + 
   geom_bar()
-
-table(es2$obst1)
-table(es2$perf1)
 
 ggplot(es2, aes(x = factor(es2$obst1), y=es2$perf1)) +  
   geom_boxplot()
@@ -102,20 +153,29 @@ ggplot(es$soe, aes(x=factor(es$soe), y=es$per)) +
   ggtitle("SOE and Performance") + 
   theme(axis.text.x = element_text(angle=90, face="bold", colour="black"))
 
-es1 <- na.omit(es)
-table(es$soe)
-is.numeric(es$soe)
-
-mosaicplot(~obst1 + car1, data = es2, main = "Survival on The Titanic", color= T) 
-
-
+mosaicplot(~obst1 + car1, data = es2, main = "Survival on The Titanic", color= T)
 
 ############################################################
 
-
-model1 <- lm(es2$perf1 ~ es2$size + es2$exporter + es2$car1 + es2$car6 + factor(es2$obst1) + factor(es2$obst2), data = es2)
+model1 <- lm(perf1 ~ country2 + stra_sector + size + exporter + car1 + car6 + 
+               factor(obst1) + factor(obst2) + factor(obst3) + factor(obst4) + factor(obst5) + factor(obst6) + factor(obst7) + factor(obst8) + factor(obst9) + factor(obst10) + factor(obst11) + factor(obst12) + factor(obst13) + factor(obst14) + factor(obst15), data = es2)
 summary(model1)
 
+model2 <- lm(perf1 ~ stra_sector + exporter + factor(obst1) + factor(obst8) + factor(obst11), data = es2)
+summary(model2)
+
+model3 <- lm(perf1 ~ exporter + factor(obst1) + factor(obst8) + factor(obst11), data = es2)
+summary(model3)
+
+library(lme4)
+
+model4 <- lmer(perf1 ~ exporter + factor(obst1) + factor(obst8) + factor(obst11) + (1|country2), data = es2)
+summary(model4)
+print(model4)
+
+model5 <- lmer(perf1 ~ exporter + factor(obst1) + factor(obst8) + factor(obst11) + (1|country2), data = es3)
+summary(model5)
+print(model5)
 
 ############################################################
 
